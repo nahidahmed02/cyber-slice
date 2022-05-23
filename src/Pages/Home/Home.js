699ac201bf46170afa0789fea82727c1807d531f
unknown <ahmednahid1995@gmail.com>
2022-05-24 04:33:20 +0600

    ready to go

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Banner from './Banner';
+import BusinessSummary from './BusinessSummary';
+import Parts from './Parts';
+import Reviews from './Reviews';
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <Parts></Parts>
+            <BusinessSummary></BusinessSummary>
+            <Reviews></Reviews>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Home/Parts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Parts = () => {
+    return (
+        <div>
+            <h2>parts</h2>
+        </div>
+    );
+};
+
+export default Parts;
