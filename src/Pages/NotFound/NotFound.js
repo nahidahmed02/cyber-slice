586c49c7abaf9d9c60c0a3f7ed1dd3a36ca81c99
unknown <ahmednahid1995@gmail.com>
2022-05-24 17:41:25 +0600

    404 route implimented

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import notFound from '../../images/404.jpg';
 
+// ---------- 404 page -------------
 const NotFound = () => {
     return (
-        <div>
-            <h2>NotFound</h2>
+        <div className='text-center mx-20 my-6'>
+            <img className='lg:mx-32' src={notFound} alt="" />
         </div>
     );
 };
