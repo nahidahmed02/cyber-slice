9c1b862dfa4cac883e1441ee253b264a019c8bdc
unknown <ahmednahid1995@gmail.com>
2022-05-26 03:24:50 +0600

    loading spinner included

---
+++ src/hooks/useParts.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+
+const useParts = () => {
+    const [parts, setParts] = useState([]);
+
+    useEffect(() => {
+
+    }, [])
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default useParts;
