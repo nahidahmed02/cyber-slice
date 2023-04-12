5dcafb4f78d0d1485e5066c2d0e2d8589503837a
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-04-12 23:55:38 +0600

    error issue solved for order quantity

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -17,7 +17,7 @@ const Purchase = () => {
 
 
     const onSubmit = data => {
-        console.log(data);
+
         const url = `http://localhost:5000/order`;
         fetch(url, {
             method: 'POST',
@@ -36,8 +36,15 @@ const Purchase = () => {
 
     return (
         <div className='mx-8 lg:mx-20 mb-8'>
-            <h2 className='underline text-center my-8 text-3xl font-bold font-serif text-amber-500'>Purchase -<span className='text-violet-700 italic ml-1'> {part.name}</span> </h2>
+
+            <h2 className='underline text-center my-8 text-3xl font-bold font-serif text-amber-500'>
+                Purchase -<span className='text-violet-700 italic ml-1'> {part.name}</span>
+            </h2>
+
+
             <div className='grid grid-cols-1 lg:grid-cols-2'>
+
+                {/* showing the part in a card */}
                 <div className="card lg:w-96 shadow-xl bg-sky-200">
                     <figure className="px-10 pt-10">
                         <img src={part.photo} alt="" className="rounded-xl" />
@@ -49,69 +56,118 @@ const Purchase = () => {
                         <p className='italic ml-2'><span className='font-bold'>Available Quantity</span>: {part.available}</p>
                         <p className='italic ml-2'><span className='font-bold'>Minimum Order Quantity</span>: {part.minOrder}</p>
                         <p className='italic ml-2'><span className='font-bold'>Price Per Unit</span>: {part.price}</p>
-
                     </div>
                 </div>
+
+                {/* form for placing the order */}
                 <div>
                     <div className='card my-6 lg:my-0 shadow-2xl'>
+
                         <div className='card-body'>
                             <h2 className='text-center my-4 text-3xl font-bold font-serif text-blue-500'>Place Order Form</h2>
-                            <form className='text-center  lg:mx-32' onSubmit={handleSubmit(onSubmit)}>
+
+                            <form className='text-center lg:mx-32' onSubmit={handleSubmit(onSubmit)}>
 
                                 <div className="form-control w-full max-w-xs">
                                     <label className="label">
                                         <span className="label-text">Your Name</span>
                                     </label>
-                                    <input {...register("name")} type="text" value={name} className='input input-bordered w-full max-w-xs' lebel readOnly />
+                                    <input
+                                        {...register("name")}
+                                        type="text"
+                                        value={name}
+                                        className='input input-bordered w-full max-w-xs'
+                                        lebel
+                                        readOnly />
                                 </div>
 
                                 <div className="form-control w-full max-w-xs">
                                     <label className="label">
                                         <span className="label-text">Your Email</span>
                                     </label>
-                                    <input {...register("email")} type="email" value={email} className='input input-bordered w-full max-w-xs' readOnly />
+                                    <input
+                                        {...register("email")}
+                                        type="email"
+                                        value={email}
+                                        className='input input-bordered w-full max-w-xs'
+                                        readOnly />
                                 </div>
 
                                 <div className="form-control w-full max-w-xs">
                                     <label className="label">
                                         <span className="label-text">Parts Name</span>
                                     </label>
-                                    <input {...register("parts")} type="text" value={part.name} className='input input-bordered w-full max-w-xs' />
+                                    <input
+                                        {...register("parts")}
+                                        type="text"
+                                        value={part.name}
+                                        className='input input-bordered w-full max-w-xs' />
                                 </div>
 
                                 <div className="form-control w-full max-w-xs">
                                     <label className="label">
-                                        <span className="label-text font-bold">Minimum Order Quantity {part.minOrder}</span>
+                                        <span className="label-text font-bold">Quantity: Min {part.minOrder}, Max {part.available}</span>
                                     </label>
-                                    <input {...register("quantity", { min: `${part.minOrder}`, max: `${part.available}` })} type="number" name="quantity" placeholder="Order Quantity" className='input input-bordered w-full max-w-xs' required />
-                                    {errors.quantity < 'min' && "Can't order less than the minimun order quantity"}
-                                    {errors.quantity > 'max' && "Can't order more than the available order quantity "}
+                                    <input
+                                        {...register("quantity", { min: part.minOrder, max: part.available })}
+                                        type="number"
+                                        name="quantity"
+                                        placeholder="Order Quantity"
+                                        className='input input-bordered w-full max-w-xs'
+                                        required />
+
+                                    {errors.quantity && errors.quantity.type === "min" && (
+                                        <span className='text-error font-bold italic'>
+                                            You can't order less than the minimum order quantity
+                                        </span>
+                                    )}
+
+                                    {errors.quantity && errors.quantity.type === "max" && (
+                                        <span className='text-error font-bold italic'>
+                                            You can't order more than the available quantity
+                                        </span>
+                                    )}
                                 </div>
 
                                 <div className="form-control w-full max-w-xs">
                                     <label className="label">
                                         <span className="label-text">Your Address</span>
                                     </label>
-                                    <input {...register("address")} type="text" name='address' placeholder='Your Address' className='input input-bordered w-full max-w-xs' required />
+                                    <input
+                                        {...register("address")}
+                                        type="text"
+                                        name='address'
+                                        placeholder='Your Address'
+                                        className='input input-bordered w-full max-w-xs'
+                                        required />
                                 </div>
 
                                 <div className="form-control w-full max-w-xs">
                                     <label className="label">
                                         <span className="label-text">Phone Number</span>
                                     </label>
-                                    <input {...register("phone")} type="text" name='phone' placeholder='Phone Number' className='input input-bordered w-full max-w-xs' required />
+                                    <input
+                                        {...register("phone")}
+                                        type="text"
+                                        name='phone'
+                                        placeholder='Phone Number'
+                                        className='input input-bordered w-full max-w-xs'
+                                        required />
                                 </div>
+
                                 {
                                     (errors.quantity < 'min' || errors.quantity > 'max')
                                         ?
-                                        <input type="submit" value="Place Order" className='btn btn-disabled w-full max-w-xs mt-4' />
+                                        <input type="submit" value="Place Order" className='btn btn-info btn-sm text-white font-bold btn-disabled w-full max-w-xs mt-4' />
                                         :
-                                        <input type="submit" value="Place Order" className='btn w-full max-w-xs mt-4' />
+                                        <input type="submit" value="Place Order" className='btn btn-info btn-sm text-white font-bold w-full max-w-xs mt-4' />
                                 }
                             </form>
                         </div>
                     </div>
+
                     <ToastContainer />
+
                 </div>
             </div>
         </div>
