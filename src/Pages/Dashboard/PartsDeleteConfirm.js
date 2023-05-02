79da18abd547f1784097875665fe2b5b0d87fec9
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-05-02 21:56:09 +0600

    button disabled for deleting products

--- src/Pages/Dashboard/PartsDeleteConfirm.js
+++ src/Pages/Dashboard/PartsDeleteConfirm.js
@@ -6,7 +6,10 @@ const PartsDeleteConfirm = ({ deleteProduct, setDeleteProduct, refetch }) => {
 
     const handleDelete = () => {
         fetch(`https://cyber-slice-server.onrender.com/parts/${_id}`, {
-            method: 'DELETE'
+            method: 'DELETE',
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            }
         })
             .then(res => res.json())
             .then(data => {
@@ -25,8 +28,9 @@ const PartsDeleteConfirm = ({ deleteProduct, setDeleteProduct, refetch }) => {
             <div className="modal modal-bottom sm:modal-middle">
                 <div className="modal-box">
                     <h3 className="font-bold text-lg text-red-500">Are you sure ?</h3>
+                    <p>Due to security reasons, CONFIRM button is kept disabled.</p>
                     <div className="modal-action">
-                        <button onClick={() => handleDelete()} className="btn btn-xs btn-error text-white">Confirm</button>
+                        <button onClick={() => handleDelete()} className="btn btn-xs btn-disabled text-white">Confirm</button>
                         <label htmlFor="delete-confirm-modal" className="btn btn-xs">Cancel</label>
                     </div>
                 </div>
