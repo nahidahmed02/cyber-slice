32172995c3e8d0d95146d60613c8b0ab5bbe7035
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-07-10 21:51:39 +0600

    loader added for loading parts

--- src/Pages/Home/Parts.js
+++ src/Pages/Home/Parts.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import useParts from '../../hooks/useParts';
 import Part from './Part';
+import Loading from '../Shared/Loading';
 
 const Parts = () => {
     const [parts] = useParts();
     return (
         <div className='mx-8 lg:mx-20 mt-12 mb-16'>
             <h2 className='text-center underline text-3xl font-bold font-serif text-amber-500'>Parts We Provide</h2>
+            {
+                parts?.length === 0 && <Loading></Loading>
+            }
             <div className='grid lg:grid-cols-2 gap-8 mt-6'>
                 {
                     parts.map(part => <Part
