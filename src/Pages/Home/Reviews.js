12a05a4aa4e7a2e4b657081c927c93f5f9dc2c48
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-08-04 20:21:46 +0600

    reviews bg color updated

--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -6,9 +6,12 @@ const Reviews = () => {
     const [reviews] = useReviews();
     return (
         <div className='lg:mx-20 mt-12 lg:mb-8' >
+
             <h2 className='text-center underline mb-6 text-3xl font-bold font-serif ' style={{ font: '#00214d' }}>Reviews</h2>
+
             <div className='' style={{ 'overflowY': 'scroll', 'height': '280px' }}>
                 <div className="overflow-x-auto">
+
                     <table className="table w-full">
                         <thead>
                             <tr style={{ color: '#00214d' }}>
@@ -18,19 +21,21 @@ const Reviews = () => {
                                 <th>Feedback</th>
                             </tr>
                         </thead>
+
                         <tbody>
                             {
                                 reviews?.length === 0 && <Loading></Loading>
                             }
                             {
                                 reviews.map((review, index) => <tr key={review._id}>
-                                    <th style={{ color: '#1b2d45' }}>{index + 1}</th>
-                                    <td style={{ color: '#1b2d45' }}>{review.name}</td>
-                                    <td style={{ color: '#1b2d45' }}>{review.rating}/5</td>
-                                    <td style={{ color: '#1b2d45' }}>{review.feedback}</td>
+                                    <th style={{ color: '#1b2d45', backgroundColor: '#fffffe' }}>{index + 1}</th>
+                                    <td style={{ color: '#1b2d45', backgroundColor: '#fffffe' }}>{review.name}</td>
+                                    <td style={{ color: '#1b2d45', backgroundColor: '#fffffe' }}>{review.rating}/5</td>
+                                    <td style={{ color: '#1b2d45', backgroundColor: '#fffffe' }}>{review.feedback}</td>
                                 </tr>)
                             }
                         </tbody>
+
                     </table>
                 </div>
             </div>
