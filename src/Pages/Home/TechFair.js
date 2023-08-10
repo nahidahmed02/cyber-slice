34efcb8761367c33d97487544c31fd6ce6364ed4
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-08-10 12:39:53 +0600

    resolved height issue of campaign section for tablet devices

--- src/Pages/Home/TechFair.js
+++ src/Pages/Home/TechFair.js
@@ -5,14 +5,14 @@ import './Style/TechFair.css'
 const TechFair = () => {
     return (
         <div
-            data-aos="flip-up"
-            data-aos-easing="linear"
-            data-aos-duration="1500"
-            className='lg:mx-20 my-10 p-6 flex items-center card-bordered bg-blue-600 rounded tech-div'>
+            // data-aos="flip-up"
+            // data-aos-easing="linear"
+            // data-aos-duration="1500"
+            className='mx-4 lg:mx-20 my-10 p-6 flex items-center card-bordered bg-blue-600 rounded tech-div'>
 
             <div className='tech-title'>
-                <p className='text-xl p-4 text-white'>A grand tech fair will be held soon at McGill University. Cyber Slice will be there too.</p>
-                <p className='text-xl p-4 text-white'>So... See you soon!</p>
+                <p className='text-lg lg:text-xl p-4 text-white'>A grand tech fair will be held soon at McGill University. Cyber Slice will be there too.</p>
+                <p className='text-lg lg:text-xl p-4 text-white'>So... See you soon!</p>
             </div>
 
             <div className='tech-img'>
